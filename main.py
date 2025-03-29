576ed06fed1f08ce0f7456724a2c70fbdd8648ae
step.sccn <sccn.sccn@yandex.ru>
2025-03-29 21:18:11 +0700

    add requirements.txt and send notification after start

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 from dotenv import load_dotenv
 import os
 from tg.handlers import start, button_handler
+from tg.tg_notifications import send_startup_message
 
 # Загружаем переменные из .env
 load_dotenv()
@@ -14,8 +15,10 @@ def main():
         raise ValueError("Токен не найден в .env файле. Убедитесь, что переменная TELEGRAM_BOT_TOKEN задана.")
 
     # Создаем приложение и регистрируем обработчики
-    application = Application.builder().token(token).build()
+    # application = Application.builder().token(token).build()
+    application = Application.builder().token(token).post_init(send_startup_message).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", start))
     application.add_handler(CallbackQueryHandler(button_handler))
 
     # Запускаем бота
--- tg/tg_notifications.py
+++ tg/tg_notifications.py
@@ -1,7 +1,10 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram.ext import Application
+from telegram.constants import ParseMode
 from utils.utils import *
-from tg.constants import TEMPLATE_NOTIFICATION_CHANNAL
+from tg.constants import TEMPLATE_NOTIFICATION_CHANNAL, BOT_START_NOTIFICATION
+
 
 async def send_notification_to_channel(update: Update, context: ContextTypes.DEFAULT_TYPE, msg:str):
     channel_id = get_channel_id()
@@ -15,3 +18,20 @@ async def send_msg_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE, 
 
 async def send_msg_to_client(update: Update, context: ContextTypes.DEFAULT_TYPE, tg_id:int, msg:str, reply_markup=None):
     await context.bot.send_message(chat_id=tg_id, text=msg, reply_markup=reply_markup)
+
+async def send_startup_message(application: Application):
+    try:
+        channel_id = get_channel_id()
+        if not channel_id:
+            print("Предупреждение: TELEGRAM_CHANNEL_ID не указан в .env, сообщение о запуске не будет отправлено")
+            return
+            
+        bot = application.bot
+        message = BOT_START_NOTIFICATION.format(date = datetime.datetime.now(), bot_id = bot.id, bot_username = bot.username)
+        await bot.send_message(
+            chat_id=channel_id,
+            text=message,
+            parse_mode=ParseMode.HTML
+        )
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения о запуске: {e}")
